986f0e852055920c0684745d1f7c9fe9095c732d
alice <bow1226@gmail.com>
2022-01-09 13:58:43 +0800

    feat: Extract 3d bounding boxes from model response

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -72,6 +72,7 @@ def load_configs_model(model_name='darknet', configs=None):
         configs.hm_size = (152, 152)
         configs.down_ratio = 4
         configs.max_objects = 50
+        configs.conf_thresh = 0.5
 
         configs.imagenet_pretrained = False
         configs.head_conv = 64
@@ -209,13 +210,22 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
-
+    score, bev_x, bev_y, z, h, bev_w, bev_l, yaw = obj
         ## step 2 : loop over all detections
-        
+        for obj in detections:
+            id, bev_x, bev_y, z, h, bev_w, bev_l, yaw = obj
+            x = bev_y / configs.bev_height * bound_size_x
+            y = bev_x / configs.bev_width * bound_size_y - bound_size_y/2.0 
+            w = bev_w / configs.bev_width * bound_size_y 
+            l = bev_l / configs.bev_height * bound_size_x
+
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
+            if ((x >= configs.lim_x[0]) and (x <= configs.lim_x[1])
+            and (y >= configs.lim_y[0]) and (y <= configs.lim_y[1])
+            and (z >= configs.lim_z[0]) and (z <= configs.lim_z[1])):
+            
             ## step 4 : append the current object to the 'objects' array
-        
+            objects.append([1, x, y, z, h, w, l, yaw])
     #######
     ####### ID_S3_EX2 START #######   
     
