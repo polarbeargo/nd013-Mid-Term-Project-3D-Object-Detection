70b719e04c0f95b6f5db6c2dfe3514b7d2d648d6
alice <bow1226@gmail.com>
2022-01-09 09:20:12 +0800

    fix: result path for resnet

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -52,19 +52,24 @@ import misc.params as params
 data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
 # data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
 # data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
-show_only_frames = [0, 1]
+# show_only_frames = [0, 1]
 # show_only_frames = [0, 200] # show only frames in interval for debugging
-
+show_only_frames = [50, 51]
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', data_filename) # adjustable path in case this script is called from another working directory
-model = "darknet"
+model = "fpn_resnet"
+result_sequence_folder = "resnet"
 sequence = "1"
-results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/' + model + '/results_sequence_' + sequence + '_' + model)
+# darknet
+# results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/' + model + '/results_sequence_' + sequence + '_' + model)
+
+# fpn_resnet
+results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/' + model + '/results_sequence_' + sequence + '_' + result_sequence_folder)
 datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
 ## Initialize object detection
-configs_det = det.load_configs(model_name='darknet') # options are 'darknet', 'fpn_resnet'
+configs_det = det.load_configs(model_name='fpn_resnet') # options are 'darknet', 'fpn_resnet'
 model_det = det.create_model(configs_det)
 
 configs_det.use_labels_as_objects = False # True = use groundtruth labels as objects, False = use model-based detection
@@ -83,10 +88,10 @@ np.random.seed(10) # make random values predictable
 ## Selective execution and visualization
 exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 ## ID_S1_EX1 settings
-exec_data = []
-exec_detection = []
-exec_tracking = [] # options are 'perform_tracking'
-exec_visualization = ['show_range_image'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+# exec_data = []
+# exec_detection = []
+# exec_tracking = [] # options are 'perform_tracking'
+# exec_visualization = ['show_range_image'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 ## ID_S1_EX2 settings
 # exec_data = []
 # exec_detection = []
@@ -100,10 +105,10 @@ exec_visualization = ['show_range_image'] # options are 'show_range_image', 'sho
 # exec_visualization=[]
 
 ## ID_S3_EX1 & ID_S3_EX2 settings
-# exec_data = ['pcl_from_rangeimage', 'load_image']
-# exec_detection = ['bev_from_pcl', 'detect_objects']
-# exec_tracking = []
-# exec_visualization = ['show_objects_in_bev_labels_in_camera']
+exec_data = ['pcl_from_rangeimage', 'load_image']
+exec_detection = ['bev_from_pcl', 'detect_objects']
+exec_tracking = []
+exec_visualization = ['show_objects_in_bev_labels_in_camera']
 
 ## ID_S4_EX1,ID_S4_EX3 settings
 # exec_data = ['pcl_from_rangeimage']
